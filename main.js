f2ca1d1aef5fcf662df3909195ea96b56de235b1
allicedimello <allicedimello@disroot.org>
2025-09-16 20:20:23 -0300

    edit:Gerando um número pseudoaleatório

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 alert("Bem vindo(a) ao jogo do número secreto!");
 
 let numeroMaximo = 100
-let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 let numeroChute;
 let numeroTentativas = 1;
 
 while (numeroChute != numeroSecreto) {
-    numeroChute = prompt("Escolha um número entre 1 e 100");
+    numeroChute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
 
     if (numeroChute == numeroSecreto) {
         alert(`Parabéns, você adivinhou o número secreto com ${numeroTentativas} tentativas!`);
