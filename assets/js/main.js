b944b1698ded583fa6886a81faf0a616357493b1
allicedimello <allicedimello@disroot.org>
2025-09-20 02:13:57 -0300

    Ativando o botão jogar novamente, corrigindo e reorganizando o código

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,50 @@
-let numeroSecreto = gerarNumero();
+let substituirCampo;
+let numeroSecreto;
 let numeroPalpite;
-let numeroMaximo = 100
-let numeroTentativas = 1;
-let mensagemNumero = `Digite um número entre 1 e ${numeroMaximo}:`
+let numeroMaximo;
+let numeroTentativas;
+let mensagemInicio;
+let mensagemInvalido;
+let maiorOuMenor;
+let mensagemErro;
+let singularOuPlural;
+let mensagemAcerto;
 
-exibitTexto("h1", "Adivinhe o número");
-exibitTexto("p", mensagemNumero);
+iniciarJogo();
+mensagemInicial();
 
-function exibitTexto(tag, texto){
-    let campo = document.querySelector(tag);
-    campo.innerHTML = texto;
+function exibirTexto(tag, texto){
+    substituirCampo = document.querySelector(tag);
+    substituirCampo.innerHTML = texto;
+}
+
+function gerarNumero(){
+    numeroMaximo = 100;
+    return parseInt(Math.random() * numeroMaximo +1);
+}
+
+function iniciarJogo(){
+    numeroSecreto = gerarNumero();
+    numeroTentativas = 1;
+    document.getElementById("jogar").removeAttribute("disabled");
+}
+
+function mensagemInicial(){
+    mensagemInicio = `Digite um número entre 1 e ${numeroMaximo}:`;
+    exibirTexto("h1", "Adivinhe o número");
+    exibirTexto("p", mensagemInicio);
 }
 
 function validarPalpite() {
-    if (numeroPalpite === "" || isNaN(numeroPalpite)) {
-        exibitTexto("p", "Digite um número válido!");
-        document.querySelector("input").value = "";
+    if (numeroPalpite == "" || isNaN(numeroPalpite)) {
+        exibirTexto("p", "Digite um número válido!");
+        limparCampo();
         return false;
     }
-    if (numeroPalpite < 1 || numeroPalpite > 100) {
-        exibitTexto("p", mensagemNumero);
-        document.querySelector("input").value = "";
+    if (numeroPalpite < 1 || numeroPalpite > numeroMaximo) {
+        mensagemInvalido = `${numeroPalpite} não é um número válido, digite um número entre 1 e ${numeroMaximo}!`
+        exibirTexto("p", mensagemInvalido);
+        limparCampo();
         return false;
     }
     return true;
@@ -29,36 +53,44 @@ function validarPalpite() {
 function verificarChute(){
     numeroPalpite = document.querySelector("input").value;
 
-     if (!validarPalpite(numeroPalpite)) {
-        document.querySelector("input").value = "";
-        return;
-    }
+    if (validarPalpite()) {
+        numeroPalpite = Number(numeroPalpite);
 
-    if (numeroPalpite == numeroSecreto){
-        exibitTexto("h1", "Acertou!");
-        let palavraTentativa = numeroTentativas > 1 ? "tentativas" : "tentativa"
-        let mensagemTentativas = `Você descobriu o número secreto com ${numeroTentativas} ${palavraTentativa}`
-        exibitTexto("p", mensagemTentativas);
+        if (numeroPalpite === numeroSecreto){
+            exibirTexto("h1", "Acertou!");
+            singularOuPlural = numeroTentativas > 1 ? "tentativas" : "tentativa"
+            mensagemAcerto = `Você descobriu o número secreto com ${numeroTentativas} ${singularOuPlural}`
+            exibirTexto("p", mensagemAcerto);
 
-        document.querySelector("input").disabled = true;
-        document.querySelector("button").disabled = true;
-    } else {
-        let palavra = numeroPalpite > numeroSecreto ? "menor" : "maior"
-        let mensagemPalpite = `O número secreto é ${palavra} que ${numeroPalpite}`
-        if (numeroPalpite > numeroSecreto){
-            exibitTexto("p", mensagemPalpite);
-        } else if (numeroPalpite < numeroSecreto) {
-            exibitTexto("p", mensagemPalpite);
+            document.getElementById("jogar").setAttribute("disabled", true);
+            document.getElementById("reiniciar").removeAttribute("disabled");
+            document.querySelector("input").disabled = true;
+
+        } else {
+            maiorOuMenor = numeroPalpite > numeroSecreto ? "menor" : "maior"
+            mensagemErro = `O número secreto é ${maiorOuMenor} que ${numeroPalpite}`
+            exibirTexto("p", mensagemErro);
+
+            numeroTentativas++
+            limparCampo();
         }
-
-        numeroTentativas++
     }
     
-    document.querySelector("input").value = "";
 }
 
-function gerarNumero(){
-    return parseInt(Math.random() * 100 +1);
+function limparCampo(){
+    numeroPalpite = document.querySelector("input");
+    numeroPalpite.value = "";
+}
+
+function reiniciarJogo(){
+    iniciarJogo();
+    mensagemInicial();
+    limparCampo();
+    document.getElementById("reiniciar").setAttribute("disabled", true);
+    document.querySelector("input").disabled = false;
+
+    console.log(numeroSecreto); // >>>>>>>>>> retirar do código
 }
 
 console.log(numeroSecreto); // >>>>>>>>>> retirar do código
