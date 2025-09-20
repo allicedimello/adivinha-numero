d30fd351775d31f496e426961d2a03c5d3357521
allicedimello <allicedimello@disroot.org>
2025-09-20 05:07:02 -0300

    edit:Ajustes de sintaxe

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,10 +5,10 @@ let numeroMaximo;
 let numeroTentativas;
 let mensagemInicio;
 let mensagemInvalido;
-let maiorOuMenor;
-let mensagemErro;
 let singularOuPlural;
 let mensagemAcerto;
+let maiorOuMenor;
+let mensagemErro;
 
 iniciarJogo();
 mensagemInicial();
@@ -20,30 +20,31 @@ function exibirTexto(tag, texto){
 
 function gerarNumero(){
     numeroMaximo = 100;
-    return parseInt(Math.random() * numeroMaximo +1);
+    return parseInt(Math.random() * numeroMaximo + 1);
 }
 
 function iniciarJogo(){
     numeroSecreto = gerarNumero();
     numeroTentativas = 1;
-    document.getElementById("jogar").removeAttribute("disabled");
+    document.getElementById('jogar').removeAttribute('disabled');
 }
 
 function mensagemInicial(){
     mensagemInicio = `Digite um número entre 1 e ${numeroMaximo}:`;
-    exibirTexto("h1", "Adivinhe o número");
-    exibirTexto("p", mensagemInicio);
+    exibirTexto('h1', 'Adivinhe o número');
+    exibirTexto('p', mensagemInicio);
 }
 
 function validarPalpite() {
-    if (numeroPalpite == "" || isNaN(numeroPalpite)) {
-        exibirTexto("p", "Digite um número válido!");
+    if (numeroPalpite === '' || isNaN(numeroPalpite)) {
+        exibirTexto('p', 'Digite um número válido!');
         limparCampo();
         return false;
     }
+
     if (numeroPalpite < 1 || numeroPalpite > numeroMaximo) {
-        mensagemInvalido = `${numeroPalpite} não é um número válido, digite um número entre 1 e ${numeroMaximo}!`
-        exibirTexto("p", mensagemInvalido);
+        mensagemInvalido = `${numeroPalpite} não é um número válido, digite um número entre 1 e ${numeroMaximo}!`;
+        exibirTexto('p', mensagemInvalido);
         limparCampo();
         return false;
     }
@@ -51,44 +52,43 @@ function validarPalpite() {
 }
 
 function verificarChute(){
-    numeroPalpite = document.querySelector("input").value;
+    numeroPalpite = document.querySelector('input').value;
 
     if (validarPalpite()) {
         numeroPalpite = Number(numeroPalpite);
 
         if (numeroPalpite === numeroSecreto){
-            exibirTexto("h1", "Acertou!");
-            singularOuPlural = numeroTentativas > 1 ? "tentativas" : "tentativa"
-            mensagemAcerto = `Você descobriu o número secreto com ${numeroTentativas} ${singularOuPlural}`
-            exibirTexto("p", mensagemAcerto);
+            exibirTexto('h1', 'Acertou!');
+            singularOuPlural = numeroTentativas > 1 ? 'tentativas' : 'tentativa';
+            mensagemAcerto = `Você descobriu o número secreto com ${numeroTentativas} ${singularOuPlural}`;
+            exibirTexto('p', mensagemAcerto);
 
-            document.getElementById("jogar").setAttribute("disabled", true);
-            document.getElementById("reiniciar").removeAttribute("disabled");
-            document.querySelector("input").disabled = true;
+            document.getElementById('jogar').setAttribute('disabled', true);
+            document.getElementById('reiniciar').removeAttribute('disabled');
+            document.querySelector('input').disabled = true;
 
         } else {
-            maiorOuMenor = numeroPalpite > numeroSecreto ? "menor" : "maior"
-            mensagemErro = `O número secreto é ${maiorOuMenor} que ${numeroPalpite}`
-            exibirTexto("p", mensagemErro);
+            maiorOuMenor = numeroPalpite > numeroSecreto ? 'menor' : 'maior';
+            mensagemErro = `O número secreto é ${maiorOuMenor} que ${numeroPalpite}`;
+            exibirTexto('p', mensagemErro);
 
-            numeroTentativas++
+            numeroTentativas++;
             limparCampo();
         }
-    }
-    
+    }  
 }
 
 function limparCampo(){
-    numeroPalpite = document.querySelector("input");
-    numeroPalpite.value = "";
+    numeroPalpite = document.querySelector('input');
+    numeroPalpite.value = '';
 }
 
 function reiniciarJogo(){
     iniciarJogo();
     mensagemInicial();
     limparCampo();
-    document.getElementById("reiniciar").setAttribute("disabled", true);
-    document.querySelector("input").disabled = false;
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+    document.querySelector('input').disabled = false;
 
     console.log(numeroSecreto); // >>>>>>>>>> retirar do código
 }
